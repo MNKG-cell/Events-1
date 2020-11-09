a138355e22f07707cbb8862d2079125c7a72fe0d
MAUREEN <maureennayebare2@gmail.com>
2020-11-09 10:17:32 +0300

    Events-

---
+++ main.js
@@ -0,0 +1,6 @@
+console.log('Hello World!');
+//instead of var btn=document. getElementbyId('btn')i used funxtions because i wanted to run in the console//
+function myFunc(){document.getElementbyId('btn')} ;
+//The block of code below is called event handler//
+  btn.onclick=function myFunc(){alert('The button was clicked and the task is to show this dialog box');}; 
+  console.log(alerted);
